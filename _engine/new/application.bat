d12df72a76597db08ec45e69a2a1bc6880c6c68d
Piotr Kosek <piotr-kosek@tlen.pl>
2017-06-27 06:22:15 +0200

    Completed new project generation

--- _engine/new/application.bat
+++ _engine/new/application.bat
@@ -4,12 +4,21 @@ pushd %~dp0
 if "%~1"=="" goto no_app_src
 set app_name="%~1"
 goto choose_type 
+
 :no_app_src
 set /p app_name=Enter project unix name you wish to add new application to:
 IF EXIST ../../%app_name% goto choose_type
 echo This project seems not to exists, choose existing one!
 goto no_app_src
+
 :choose_type
+echo Do you wish to create sample application, or start with blank project?
+echo 1 - Blank application
+echo 2 - Launcher
+echo 3 - Editor
+choice /N /C:123 /M "Choose one"
+if errorlevel ==3 goto type_editor
+if errorlevel ==2 goto type_launcher
 echo Enter type of your application:
 echo 1 - Executable
 echo 2 - Shared library
@@ -20,24 +29,56 @@ if errorlevel ==2 goto type_shared
 if errorlevel ==1 goto type_exe
 echo Wrong answer. Please try again.
 goto choose_type
+
+:type_editor
+set app_predefined=editor
+set app_type=executable
+goto after_type
+:type_launcher
+set app_predefined=launcher
+set app_type=executable
+goto after_type
 :type_exe
+set app_predefined=executable
 set app_type=executable
 goto after_type
 :type_shared
+set app_predefined=shared_library
 set app_type=shared_library
 goto after_type
 :type_test
+set app_predefined=test
 set app_type=test
 goto after_type
 :after_type
 
+if %app_type%==executable (
+:choose_launch
+echo Do you wish to make this app launch as default?
+echo 1 - Yes
+echo 2 - No
+choice /N /C:12 /M "Choose one"
+if errorlevel ==2 goto launch_no
+if errorlevel ==1 goto launch_yes
+echo Wrong answer. Please try again.
+goto choose_launch
+:launch_yes
+set app_make_default=true
+goto after_launch
+:launch_no
+set app_make_default=false
+goto after_launch
+)
+
+:after_launch
 set /p unix_filename=Enter unix filename of your new application (lowercase, no spaces nor special characters): 
 set /p full_filename=Enter full name of your application (should be a valid filename!): 
 cd ../depedency/apache_ant/bin
 set path=%~dp0../depedency/jdk1.7.0_71/bin;%~dp0../depedency/mingw/bin;%path%
 echo on
-call ant -v -buildfile "../../../new/ant.xml" new_application -Dunix="%unix_filename%" -Dfull="%full_filename%" -Dapp_type="%app_type%" -Dapp_src="%app_name%"
+call ant -v -buildfile "../../../new/ant.xml" new_application -Dunix="%unix_filename%" -Dfull="%full_filename%" -Dapp_type="%app_type%" -Dapp_src="%app_name%" -Dapp_predefined="%app_predefined%" -Dapp_make_default="%app_make_default%"
 cd ../../../../%app_name%
 call update_vs_solution.bat
+
 popd
 pause
--- _engine/new/project/create_new_application.bat
+++ _engine/new/project/create_new_application.bat
@@ -1,5 +1,4 @@
 @echo off
 pushd %~dp0
 call "./../_engine/new/application.bat" "$EMPTY_UNIX_NAME$"
-call "./update_vs_solution.bat"
 popd
