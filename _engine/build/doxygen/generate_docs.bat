6b18f402fe22e2945fe71e7237e9c5897a444803
Piotr Kosek <piotr-kosek@tlen.pl>
2017-08-06 16:33:51 +0200

    Working on doxyfile for os_x

--- _engine/build/doxygen/generate_docs.bat
+++ _engine/build/doxygen/generate_docs.bat
@@ -1,6 +1,7 @@
 pushd %~dp0
 rmdir /s /q ..\..\_build\doxygen
 mkdir ..\..\_build\doxygen
+cd windows
 set current_directory=%CD%
-"../../depedency/doxygen/doxygen.exe" doxyfile
+"../../../depedency/doxygen/doxygen.exe" doxyfile
 popd
