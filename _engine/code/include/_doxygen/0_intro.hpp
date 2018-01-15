b6421e702091e768fbdf200d8c4393424a3e42f0
Piotr Kosek <piotr-kosek@tlen.pl>
2018-01-15 18:49:35 +0100

    WiP of main page in documentation

--- _engine/code/include/_doxygen/0_intro.hpp
+++ _engine/code/include/_doxygen/0_intro.hpp
@@ -27,53 +27,18 @@
 *     Written in modern C++. Powerful, flexible and extendable. And completely stand-alone!
 *
 * @section pixie_features Features
-* @subsection pixie_features_component Components
-*     - @ref components "Build based on components". Each can be disabled, replacing core functionality with dummy class
-*     - Major components
-*         - @ref engine::config_t "Configuration component" with real-time config update notfiications
-*         - @ref engine::data_source_t "Data provider" monitoring VFS for real-time data files changes
-*         - @ref engine::logger_t "Logger" allowing multiple logging outputs, formatting and raporting to user
-*         - @ref engine::terminal_writer_t "Terminal writer" to standard output (usually the system console)
-*     - Minor components
-*         - @ref engine::environment_info_t "Environmnet info" provider for logging purpouses
-*         - @ref engine::renderer_status_t "Frame Notifier" allowing querying data for renderer status
-*         - @ref engine::logger_file_writer_t "Log file writer" for quick and robust log output, available even when full @ref engine::data_provieder_t "data provider" is unavailable
-* @subsection pixie_features_provider Information providers
-*     - @ref engine::common_filenames_provider_t "Common filenames" on given platform (like logger output filename)
-*     - @ref engine::save_location_provider_t "Save location" for given platform (full path)
-*     - @ref engine::scanners_provider_t "Scanner provider" for @ref virtual_path VFS on given platform
-* @subsection pixie_features_utilities Utilities
-*     - Concurrention
-*         - @ref engine::thread_pool_t "Thread pool"
-*     - Containers
-*         - @ref engine::compile_time_map_t "Compile-time key-value map"
-*         - @ref engine::concurrent_queue_t "Concurrent queue"
-*         - @ref engine::sync_queue_t "Sync queue"
-*         - @ref engine::sync_vector_t "Sync vector"
-*     - @ref data "Data input/output utilities"
-*     - Debugging tools
-*         - @ref engine::platform::dump_callstack "Callstack dumps"
-*     - @ref messenger "Multithreaded messenger" for internal components communication
-*     - Design patterns
-*         - @ref settings "Compile-time engine-wide settings"
-*         - @ref engine::factory_t "Abstract Factory"
-*         - @ref engine::flags_t "Flags" (based on enum classes)
-*         - @ref engine::id_t "FourCC IDs"
-*         - @ref engine::instance_lifetime_guard "Lifetime guards" (for platforms that cannot handle stack unwinding during exceptions)
-*         - @ref components_provider "Providers subsystem for components"
-*     - Platform
-*         - @ref engine::compilation_t "Compilation-time switches"
-*     - Renderer helpers
-*         - @ref engine::color_t "sRGB color class"
-*     - Security helpers
-*         - @ref engine::crc32_t "Fast CRC32 calculator"
-*     - Text utilities
-*         - @ref engine::ustring_t "UTF-8 strings"
-*         - @ref ustring_format "Text formatters"
+* @subsection pixie_features_architecture Architecture
+*     - @ref components "Architecture based on components". Each can be disabled, replacing core functionality with dummy class (for example for testing purposes)
+*     - @ref providers "Most of the components supports different functionality providers", which are tweakable
+*     - Major group of components
+*         - @ref component_config "Configuration component" with real-time config update notifications
+*         - @ref component_data "Data provider component" providing VFS over files across different systems
+*         - @ref component_environment "Environment component" providing insight on executing environment
+*         - @ref component_filesystem "Filesystem component" providing direct access to files (outside Data provider)
+*         - @ref component_logger "Logger" supporting variety of output formats (even scriptable by user!)
+*         - @ref component_terminal "Terminal component" allowing multiple terminal window with text coloring on each supported platform
 * @subsection pixie_features_other Other features
-*     - @ref settings "Compile-time engine settings"
-*     - @ref bootstrapper "Bootstrapper for initialization"
-*     - ~10,000 LoC as of August, 1st, 2017
+*     - ~18,000 LoC across 225 files as of January, 1st, 2018
 *
 * @section pixie_platforms Supported Platforms
 * @subsection pixie_platforms_win Windows XP/Vista/7/8/8.1/10
@@ -85,7 +50,7 @@
 * @subsection pixie_platforms_osx OS X
 *    (Expected, not yet working)
 *
-* @section pixe_depedencies Depedencies
+* @section pixie_depedencies Depedencies
 *     - @ref dependency_angelcode "AngelCode"
 *     - @ref dependency_anttweakbar "AntTweakBar"
 *     - @ref dependency_apacheant "Apache Ant"
@@ -114,6 +79,6 @@
 *     - @ref dependency_upx "UPX"
 *     - @ref dependency_wxwidgets "wxWidgets"
 *
-* @section pixe_copyrights Copyrights
+* @section pixie_copyrights Copyrights
 *    This engine is written and maintained by Kosek.com
 */
