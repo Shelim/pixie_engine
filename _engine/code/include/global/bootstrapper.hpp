e6955708473a1925084eceb29f5c721f83198e73
Piotr Kosek <piotr-kosek@tlen.pl>
2018-01-24 19:46:42 +0100

    Fixed compilation problems in bootstrapper

--- _engine/code/include/global/bootstrapper.hpp
+++ _engine/code/include/global/bootstrapper.hpp
@@ -47,68 +47,73 @@
 
 namespace engine
 {
+
+    namespace global
+    {        
 	
-	namespace
-	{
-		
-		class shared_scope {
-		public:
-			template <class, class T>
-			class scope {
+		namespace
+		{
+			
+			class shared_scope {
 			public:
-				template <class T_>
-				using is_referable = typename boost::di::wrappers::shared<shared_scope, T>::template is_referable<T_>;
+				template <class, class T>
+				class scope {
+				public:
+					template <class T_>
+					using is_referable = typename boost::di::wrappers::shared<shared_scope, T>::template is_referable<T_>;
 
-				template <class, class, class TProvider>
-				static auto try_create(const TProvider& provider)
-					-> decltype(boost::di::wrappers::shared<shared_scope, T>{std::shared_ptr<T>{provider.get()}});
+					template <class, class, class TProvider>
+					static auto try_create(const TProvider& provider)
+						-> decltype(boost::di::wrappers::shared<shared_scope, T>{std::shared_ptr<T>{provider.get()}});
 
-				template <class, class, class TProvider>
-				auto create(const TProvider& provider) {
-					if (!object_) {
-						object_ = std::shared_ptr<T>{ provider.get() };
+					template <class, class, class TProvider>
+					auto create(const TProvider& provider) {
+						if (!object_) {
+							object_ = std::shared_ptr<T>{ provider.get() };
+						}
+						return boost::di::wrappers::shared<shared_scope, T>{object_};
 					}
-					return boost::di::wrappers::shared<shared_scope, T>{object_};
-				}
 
-			private:
-				std::shared_ptr<T> object_;
+				private:
+					std::shared_ptr<T> object_;
+				};
 			};
-		};
 
-		static constexpr shared_scope shared{};
+			static constexpr shared_scope shared{};
 
 
-		auto injector_dummies = [] {
-			return boost::di::make_injector(
-#define ENGINE_COMPONENT_DEF(component)	boost::di::bind<component##_t>().in(shared).to <component##_dummy_t>(),
-#include "def/component.def"
-#define ENGINE_FACTORABLE_DEF(factorable) boost::di::bind<ifactory<factorable>>().to(factory<factorable>{}),
-#include "def/factorable.def"
-				boost::di::make_injector());
-		};
+			auto injector_dummies = [] {
+				return boost::di::make_injector(
+	#define ENGINE_COMPONENT_DEF(component)	boost::di::bind<component##_t>().in(shared).to <component##_dummy_t>(),
+	#include "def/component.def"
+	#define ENGINE_FACTORABLE_DEF(factorable) boost::di::bind<ifactory<factorable>>().to(factory<factorable>{}),
+	#include "def/factorable.def"
+					boost::di::make_injector());
+			};
 
-		auto injector_context = [] (global::app_context_t* context){ // ToDo: register instances for global components
-			return boost::di::make_injector(
-/*
-#define ENGINE_COMPONENT_DEF(component)	boost::di::bind<component##_t>().in(shared).to <component##_dummy_t>(),
-#include "def/component.def"
-*/
-				boost::di::make_injector());
-		};
+			auto injector_context = [] (global::app_context_t* context){ // ToDo: register instances for global components
+				return boost::di::make_injector(
+	/*
+	#define ENGINE_COMPONENT_DEF(component)	boost::di::bind<component##_t>().in(shared).to <component##_dummy_t>(),
+	#include "def/component.def"
+	*/
+					boost::di::make_injector());
+			};
+		}
+
 	}
 }
 
 #define INTERNAL_PROVIDER_INIT(provider_type, ...) boost::di::bind<engine::holder_t<engine::provider_type##_t>>().to < engine::holder_implementation_t<engine::provider_type##_t 
 
-#define BEGIN_BOOTSTRAPPER(name, context) const auto name = boost::di::make_injector(engine::injector_context(context.get()), 
-#define ENABLE_COMPONENT(component) boost::di::bind<engine::component##_t>().in(engine::shared).to <engine::component##_real_t>()[boost::di::override],
-#define MOCKUP_COMPONENT(component) boost::di::bind<engine::component##_t>().in(engine::shared).to <engine::component##_mockup_t>()[boost::di::override],
+#define BEGIN_BOOTSTRAPPER(name, context) const auto name = boost::di::make_injector(engine::global::injector_context(context.get()), 
+#define ENABLE_COMPONENT(component) boost::di::bind<engine::component##_t>().in(engine::global::shared).to <engine::component##_real_t>()[boost::di::override],
+#define MOCKUP_COMPONENT(component) boost::di::bind<engine::component##_t>().in(engine::global::shared).to <engine::component##_mockup_t>()[boost::di::override],
 #define USE_SETTINGS(owner, id) boost::di::bind<engine::settings_t<engine::owner> >().to <engine::settings_implementation_t<engine::owner, engine::settings_metadata_##owner##_##id##_t> >(),
 #define USE_PROVIDER_FOR(provider_type, provider) boost::di::bind<engine::holder_t<engine::provider_type##_t>>().to < engine::holder_implementation_t<engine::provider_type##_t INTERNAL_PROVIDER_1(provider_type, provider)> >(),
 #define USE_PROVIDERS_FOR(...) EXPAND(INTERNAL_PROVIDER_INIT(__VA_ARGS__)) EXPAND(EXPAND(_GET_NTH_ARG(__VA_ARGS__, INTERNAL_PROVIDER_9, INTERNAL_PROVIDER_8, INTERNAL_PROVIDER_7, INTERNAL_PROVIDER_6, INTERNAL_PROVIDER_5, INTERNAL_PROVIDER_4, INTERNAL_PROVIDER_3, INTERNAL_PROVIDER_2, INTERNAL_PROVIDER_1, INTERNAL_PROVIDER_0))(__VA_ARGS__)) > >(),
 #define APP(app_id) boost::di::bind<engine::manifest_app_t::app_t>().to(engine::manifest_app_t::app_t::app_id),
-#define END_BOOTSTRAPPER() engine::injector_dummies() );
+#define END_BOOTSTRAPPER() engine::global::injector_dummies() );
 
 #define GET_COMPONENT(bootstrapper, component) bootstrapper.create<std::shared_ptr<engine::component##_t> >()
 
