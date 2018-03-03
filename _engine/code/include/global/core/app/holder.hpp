8a2eaacea968c37d5648efbdf495a45b39f5e2ec
Piotr Kosek <piotr-kosek@tlen.pl>
2018-03-03 23:55:07 +0100

    Working on App Holder

--- _engine/code/include/global/core/app/holder.hpp
+++ _engine/code/include/global/core/app/holder.hpp
@@ -10,22 +10,19 @@
 
 namespace engine
 {
-    /*
-
     class app_holder_t
     {
 
         public:
 
-            app_holder_t(std::unique_ptr<app_t> app, std::unique_ptr<app_context_t> context) : app(std::move(app)), context(std::move(context)), return_code(app_t::return_code_t::still_running), thread([this]{this->run_app();})
+            app_holder_t(std::unique_ptr<app_t> app, std::unique_ptr<app_context_t> context, std::shared_ptr<thread_factory_t> thread_factory) : app(std::move(app)), context(std::move(context)), return_code(app_t::return_code_t::still_running)
             {
-                
+                thread = thread_factory->create(this->app->get_app(), this->context->get_instance_id(), format_string("App #1####2# main thread"_u, this->app->get_app(), this->context->get_instance_id()), [this]{this->run_app();});
             }
 
             ~app_holder_t()
             {
                 wait_till_completed();
-                thread.join();
             }
 
             app_t::return_code_t get_return_code()
@@ -36,24 +33,6 @@ namespace engine
             void wait_till_completed()
             {
                 signal_for_completion.wait();
-            }
-
-            enum class result_t
-            {
-                successful,
-                rejected
-            };
-
-            result_t terminate(app_signal_terminate_t::reason_t reason)
-            {
-                app_signal_terminate_t signal_terminate(reason);
-                send_signal(&signal_terminate);
-
-                if(signal_terminate.is_cancelled())
-                    return result_t::rejected;
-
-                wait_till_completed();
-                return result_t::successful;
             }
 
             void send_signal(app_signal_t * app_signal)
@@ -74,12 +53,11 @@ namespace engine
 
             app_t::return_code_t return_code;
 
-            std::thread thread;
+			std::unique_ptr<thread_t> thread;
             signal_t signal_for_completion;
 
     };
-
-    */
+    
 }
 
 
