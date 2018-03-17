d5d5fa3b5254e6ad6dbcc3ecc4d46450367d1e86
Piotr Kosek <piotr-kosek@tlen.pl>
2018-03-17 10:41:58 +0100

    Bootstrapper now has separate object that contains get_XXX() methods

--- _engine/code/include/global/core/app/bootstrapper.hpp
+++ _engine/code/include/global/core/app/bootstrapper.hpp
@@ -114,6 +114,41 @@ namespace engine
 				boost::di::bind<app_t::instance_id_t>().named(engine::instance_id).to(context->get_instance_id()),
 				boost::di::bind<>().to(context));
 		};
+
+		class boostrapper_t
+		{
+
+		public:
+
+			class unused_t
+			{
+
+			};
+
+			boostrapper_t(boost::di::injector<
+#define ENGINE_GLOBAL_COMPONENT_DEF(component) std::shared_ptr<component##_t>,
+#include "def/global_component.def"
+#define ENGINE_COMPONENT_DEF(component) std::shared_ptr<component##_t>,
+#include "def/component.def"
+			unused_t> injector) : injector(std::move(injector))
+			{
+
+			}
+#define ENGINE_GLOBAL_COMPONENT_DEF(component) std::shared_ptr<engine::component##_t> get_##component() { return injector.create<std::shared_ptr<engine::component##_t> >(); }
+#include "def/global_component.def"
+#define ENGINE_COMPONENT_DEF(component) std::shared_ptr<engine::component##_t> get_##component() { return injector.create<std::shared_ptr<engine::component##_t> >(); }
+#include "def/component.def"
+
+		private:
+		
+			boost::di::injector<
+#define ENGINE_GLOBAL_COMPONENT_DEF(component) std::shared_ptr<component##_t>,
+#include "def/global_component.def"
+#define ENGINE_COMPONENT_DEF(component) std::shared_ptr<component##_t>,
+#include "def/component.def"
+			unused_t> injector;
+
+		};
 	}
 
 }
@@ -121,7 +156,7 @@ namespace engine
 
 #define INTERNAL_PROVIDER_INIT(provider_type, ...) boost::di::bind<engine::holder_t<engine::provider_type##_t>>().to < engine::holder_implementation_t<engine::provider_type##_t 
 
-#define BEGIN_BOOTSTRAPPER(name, context) const auto name = boost::di::make_injector(engine::injector_context(context), 
+#define BEGIN_BOOTSTRAPPER(name, context) auto name = engine::boostrapper_t(std::move(boost::di::make_injector(engine::injector_context(context), 
 #define ALL_COMPONENTS_BY_DEFAULT_ARE(type) engine::injector_##type(),
 #define ENABLE_COMPONENT(component) boost::di::bind<engine::component##_t>().in(engine::shared).to <engine::component##_real_t>()[boost::di::override],
 #define MOCKUP_COMPONENT(component) boost::di::bind<engine::component##_t>().in(engine::shared).to <engine::component##_mockup_t>()[boost::di::override],
@@ -130,9 +165,7 @@ namespace engine
 #define USE_PROVIDER_FOR(provider_type, provider) boost::di::bind<engine::holder_t<engine::provider_type##_t>>().to < engine::holder_implementation_t<engine::provider_type##_t INTERNAL_PROVIDER_1(provider_type, provider)> >(),
 #define USE_PROVIDERS_FOR(...) EXPAND(INTERNAL_PROVIDER_INIT(__VA_ARGS__)) EXPAND(EXPAND(_GET_NTH_ARG(__VA_ARGS__, INTERNAL_PROVIDER_9, INTERNAL_PROVIDER_8, INTERNAL_PROVIDER_7, INTERNAL_PROVIDER_6, INTERNAL_PROVIDER_5, INTERNAL_PROVIDER_4, INTERNAL_PROVIDER_3, INTERNAL_PROVIDER_2, INTERNAL_PROVIDER_1, INTERNAL_PROVIDER_0))(__VA_ARGS__)) > >(),
 #define APP(app_id) boost::di::bind<engine::app_t::kind_t>().to(engine::app_t::kind_t::app_id),
-#define END_BOOTSTRAPPER() engine::injector_factorable() );
-
-#define GET_COMPONENT(bootstrapper, component) bootstrapper.create<std::shared_ptr<engine::component##_t> >()
+#define END_BOOTSTRAPPER() engine::injector_factorable() )));
 
 #include "global/core/app/bootstrapper_standard.hpp"
 
