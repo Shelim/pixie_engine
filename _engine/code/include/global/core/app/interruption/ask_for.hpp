e5ce2882ed7848a09aa49d62f10abc79589842e2
Piotr Kosek <piotr-kosek@tlen.pl>
2018-03-17 14:38:10 +0100

    Interruption -> ask_for now has handle that can be extracted, if question in question would take too much time (ie. requiring user to do something)

--- _engine/code/include/global/core/app/interruption/ask_for.hpp
+++ _engine/code/include/global/core/app/interruption/ask_for.hpp
@@ -15,25 +15,94 @@ namespace engine
 
             typedef std::function<void()> callback_t;
 
+            class handle_t
+            {
+                public:
+
+                    handle_t(default_t default_is, callback_t on_allowed = [](){}, callback_t on_refused = [](){}) : default_is(default_is), on_allowed(on_allowed), on_refused(on_refused)
+                    {
+
+                    }
+
+                    ~handle_t()
+                    {
+                        execute_unhandled_handler();
+                    }
+
+                    void allow()
+                    {
+                        if(!was_handled())
+                        {
+                            mark_as_handled();
+                            on_allowed();
+                        }
+                    }
+
+                    void refuse()
+                    {
+                        if(!was_handled())
+                        {
+                            mark_as_handled();
+                            on_refused();
+                        }
+                    }
+
+                    void execute_unhandled_handler() 
+                    {
+                        switch(default_is)
+                        {
+                            case default_t::allowed: allow(); break;
+                            case default_t::refused: refuse(); break;
+                        }
+                    }
+
+                    bool was_handled()
+                    {
+                        return flags.is_flag(flag_t::was_handled);
+                    }
+                
+                private:
+
+                    enum class flag_t
+                    {
+                        was_handled,
+                        count
+                    };
+
+                    flags_t<flag_t> flags;
+
+                    void mark_as_handled()
+                    {
+                        flags.set_flag(flag_t::was_handled, true);
+                    }
+
+                    default_t default_is;
+                    callback_t on_allowed;
+                    callback_t on_refused;
+            };
+
             void allow()
             {
                 mark_as_handled();
-                on_allowed();
+                if(handle) handle->allow();
             }
 
             void refuse()
             {
                 mark_as_handled();
-                on_refused();
+                if(handle) handle->refuse();
             }
 
             void execute_unhandled_handler() final
             {
-                switch(default_is)
-                {
-                    case default_t::allowed: allow(); break;
-                    case default_t::refused: refuse(); break;
-                }
+                mark_as_handled();
+                if(handle) handle->execute_unhandled_handler();
+            }
+           
+            std::unique_ptr<handle_t> extract_handle_for_later_use()
+            {
+                mark_as_handled();
+                return std::move(handle);
             }
 
         protected:
@@ -44,15 +113,14 @@ namespace engine
                 refused
             };
 
-            interruption_ask_for_t(type_t type, app_t::instance_id_t target, default_t default_is, callback_t on_allowed = [](){}, callback_t on_refused = [](){}) : interruption_t(type, target), default_is(default_is), on_allowed(std::move(on_allowed)), on_refused(std::move(on_refused))
+            interruption_ask_for_t(type_t type, app_t::instance_id_t target, default_t default_is, callback_t on_allowed = [](){}, callback_t on_refused = [](){}) : interruption_t(type, target), handle(std::make_unique<handle_t>(default_is, std::move(on_allowed), std::move(on_refused)))
             {
 
             }
 
         private:
-            default_t default_is;
-            callback_t on_allowed;
-            callback_t on_refused;
+           
+            std::unique_ptr<handle_t> handle;
 
     };
 
