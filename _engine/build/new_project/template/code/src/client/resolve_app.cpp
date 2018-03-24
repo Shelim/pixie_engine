46bf23f1ae0d64070ad66588d36e245fa9c59c30
Piotr Kosek <piotr-kosek@tlen.pl>
2018-03-24 08:12:59 +0100

    Updated missing change in client code

--- _engine/build/new_project/template/code/src/client/resolve_app.cpp
+++ _engine/build/new_project/template/code/src/client/resolve_app.cpp
@@ -5,7 +5,7 @@ engine::app_t::kind_t engine::client::resolve_app(app_context_t * context)
     return engine::app_t::kind_t::game; // ToDo: This should be populated from context above!!!
 }
 
-engine::args_t engine::client::startup_args_to_run_new_instance_of(app_t::kind_t kind)
+engine::args_t engine::client::startup_args_to_run_new_instance_of(app_t::kind_t kind, args_t original_args)
 {
-    return engine::args_t("");  // ToDo: This should be populated from kind above!!!
+    return original_args;  // ToDo: This should be populated from kind above!!!
 }
